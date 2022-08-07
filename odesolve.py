e5aa0ec15394c8499239eb8f48fd9d38f943a507
vm20381 <vm20381@bristol.ac.uk>
2022-08-07 23:30:11 +0100

    adding new code to odesolve.py

--- odesolve.py
+++ odesolve.py
@@ -1,24 +1,48 @@
 # odesolve.py
 #
-# Author: <insert name>
-# Date:   <insert date>
-# Description: <insert description>
+# Author: <Nicholas Saremi>
+# Date:   <01/08/2022>
+# Description: <Using Numerical Methods (Euler and Runge Kutta) to solve Ordinary Differential Equations>
 #
 # You should fill out the code for the functions below so that they pass the
 # tests in test_odesolve.py
 
 def euler(f, x, t, h):
-    """Perform one step of the Euler method"""
+    return x + f(x , t )*h
     pass
 
 
 def rk4(f, x, t, h):
-    """Perform one step of the RK$ method"""
+    k1 = f(x, t)
+    k2 = f(x + k1*(h/2), t + (h/2))
+    k3 = f(x + k2*(h/2), t + (h/2))
+    k4 = f(x + k3*h, t + h)
+    return x + (k1 + 2*k2+ 2*k3 + k4)*(h/6)
+
+	
+	
     pass
 
+def solveto(f, x0, t1, t2, hmax):      #, method=euler
+    for i in range (0,_total_jumps):           
+        x0= euler(f, x0, t1, hmax)
+        t1 = t1 + hmax
+        x1=x0
+        #print(i)
+    
+        if i ==_total_jumps - 2 :
+            hmax = hmin
+        if t1 == t2:
+            #print(x1)
+            break
+    return x1
+    pass
 
-def solveto(f, x1, t1, t2, hmax, method=euler):
-    """Use many steps of method to get from x1,t1 to x2,t2"""
+solveto(f, x0, t1, t2, hmax)   
+
+
+
+"""Use many steps of method to get from x1,t1 to x2,t2"""
     pass
 
 
